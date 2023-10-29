5e885e165eac31521fec3ba4213eda957476e74f
Nadezhda Zhuzhleva <nzhuzhle@car14s4.42barcelona.com>
2023-10-29 19:51:02 +0100

    updated lexer

--- src/lexer/lexer_main.c
+++ src/lexer/lexer_main.c
@@ -3,14 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   lexer_main.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plinscho <plinscho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nzhuzhle <nzhuzhle@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 18:33:36 by nzhuzhle          #+#    #+#             */
-/*   Updated: 2023/10/29 17:41:44 by plinscho         ###   ########.fr       */
+/*   Updated: 2023/10/25 18:33:52 by nzhuzhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
+#include "lexer.h"
+
+t_lexer *read_space(char *in, int *i)
+{
+	int	j;
+
+	j = 0;
+	while (in[j + 1] && in[j + 1] == ' ')
+        j++;
+	*i += j;
+    return(lex_new(NULL, 0));
+}
+/* I dont interpret the \\ like in bash -- see the subject */
+t_lexer *read_word(char *in, int *i)
+{
+	char	*cont;
+	int		j;
+
+	j = 0;
+	while (in[j] && in[j + 1] != ' ')
+		j++;
+	cont = ft_substr_quotes(in, 0, j, -1);
+	if (!cont)
+		return (NULL);
+	*i += j;
+	return (lex_new(cont, 1));
+}
+
+/* I dont interpret the \\ like in bash -- see the subject */
+t_lexer *read_in_quotes(char *in, int *i)
+{
+	char	*cont;
+	int		j;
+
+	j = 0;
+//	printf("entered read quotes: i == %i\n", *i);
+	while (in[j] && in[j + 1] != in[0])
+		j++;
+	cont = ft_substr(in, 1, j);
+	if (!cont)
+		return (NULL);
+	*i += j + 1;
+//	printf("in read quotes: i == %i\n", *i);
+	if (in[0] == '\'')
+		return (lex_new(cont, 2));
+	else
+		return (lex_new(cont, 3));
+	
+}
 
 t_lexer *read_redirection(char *in, int *i)
 {
@@ -23,12 +71,12 @@ t_lexer *read_redirection(char *in, int *i)
         return (lex_new(NULL, 5));
     else if (in[j] == '<' && in[j + 1] == '<') // the case of << heredoc
     {
-        *i++;
+        (*i)++;
         return (lex_new(NULL, 6));
     }
-    else if (in[j] == '<' && in[j + 1] == '<') // the case of >> append
+    else if (in[j] == '>' && in[j + 1] == '>') // the case of >> append
     {    
-        *i++;
+        (*i)++;
         return (lex_new(NULL, 7));
     }
     else if (in[j] == '|')
@@ -36,47 +84,59 @@ t_lexer *read_redirection(char *in, int *i)
     return (NULL);
 }
 
-
-/*	P: what happens with the "" and ''? There is no case where the program saves the
-	token into the list. Or if the input has '\' we have to ingnore the next character.
-	We need to consider these cases before tokenizing.
-*/
-t_lexer *lexer(char *input)
+int lexer(char *input, t_lexer **head)
 {
-    t_lexer *list;
     t_lexer *new;
     int i;
 
     i = -1;
-    list = NULL;
     new = NULL;
     while (input[++i])
     {
         if (input[i] == ' ')
-        {
-            while (input[i + 1] && input[i + 1] == ' ')
-                i++;
-            new = lex_new(NULL, 0);
-        }
+            new = read_space(&input[i], &i);
         else if (input[i] == '<' || input[i] == '>' || input[i] == '|')
             new = read_redirection(&input[i], &i);
-        
+        else if (input[i] == '\'' || input[i] == '\"')
+			new = read_in_quotes(&input[i], &i);
+		else
+			new = read_word(&input[i], &i);
+		if (!new)
+			return (lex_clean(head, &input));
+		else
+			lex_add(head, new);
     }
+	return (0);
 }
-/*
-int main(int argc, char **argv)
+
+int main(int argc, char **argv, char **envp)
 {
-    t_lexer *list;
-    int i;
+    char    *input = NULL;
+	t_lexer *lex_list = NULL;
+	char	**cp_env = NULL; 
+ //   int i;
 
-    i == 1;
-    list = lexer(argv[1]);
-    while (list)
-    {
-        printf("node 1 -- content: %s, type; %i\n", list->cont, list->token); //erase
-        i++;
-        list == list->next; 
-    }
-
+	if (argc != 1 && argv[0])
+		return (1);
+	cp_env = cpy_env(envp);
+	if (!cp_env)
+		return (1);
+	while (42)
+	{
+		input = readline("minishell> ");  
+        if (!input) // in case we recieved an empty line
+            break;
+		printf("[MAIN]You entered: %s\n\n", input);
+		if (lexer(input, &lex_list)) // it means that a malloc failed, my lex_clean cleaned input and list
+			return (1);
+		/*i = 1;
+    	while (lex_list)
+    	{
+			printf("node %i -- content: %s, type; %i\n", i, lex_list->cont, lex_list->token); //erase
+       	 	i++;
+        	lex_list = lex_list->next; 
+    	}*/
+	}
+	lex_clean(&lex_list, &input);
+	return (0);
 }
-*/
--- src/parser/parcer_main.c
+++ src/parser/parcer_main.c
@@ -1,19 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.h                                           :+:      :+:    :+:   */
+/*   parcer_main.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nzhuzhle <nzhuzhle@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/29 17:00:51 by nzhuzhle          #+#    #+#             */
-/*   Updated: 2023/10/29 19:50:21 by nzhuzhle         ###   ########.fr       */
+/*   Created: 2023/10/29 17:02:54 by nzhuzhle          #+#    #+#             */
+/*   Updated: 2023/10/29 17:03:15 by nzhuzhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_H
-# define PARSER_H
+#include "parser.h"
 
-# include "../../include/minishell.h"
-# include "../lexer/lexer.h"
 
-#endif
