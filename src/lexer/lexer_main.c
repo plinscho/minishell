ce2299efa6275b66cc505faba5819de796503fc8
Nadezhda Zhuzhleva <nzhuzhle@car12s5.42barcelona.com>
2023-11-01 18:28:24 +0100

    try

--- src/lexer/lexer_main.c
+++ src/lexer/lexer_main.c
@@ -27,11 +27,23 @@ t_lexer *read_word(char *in, int *i)
 {
 	char	*cont;
 	int		j;
+	char	q;
 
 	j = 0;
-	while (in[j] && in[j + 1] != ' ')
+	while (in[j] && (in[j] != ' ' &in[j] != '\'' && in[j] != '\"'))
 		j++;
-	cont = ft_substr_quotes(in, 0, j, -1);
+	if (in[j] && (in[j] == '\'' || in[j] == '\"'))
+	{
+		q = in[j];
+		j++;
+		while (in[j] && in[j] != q)
+			j++;
+		while (in[j] && in[j + 1] && in[j + 1] != ' ')
+			j++;
+		cont = ft_substr_quotes(in, q, j, -1);
+	}
+	else
+		cont = ft_substr(in, 0, j - 1);
 	if (!cont)
 		return (NULL);
 	*i += j;
@@ -113,14 +125,14 @@ int main(int argc, char **argv, char **envp)
 {
     char    *input = NULL;
 	t_lexer *lex_list = NULL;
-	char	**cp_env = NULL; 
- //   int i;
+//	char	**cp_env = NULL; 
+    int i;
 
 	if (argc != 1 && argv[0])
 		return (1);
-	cp_env = cpy_env(envp);
-	if (!cp_env)
-		return (1);
+//	cp_env = cpy_env(envp);
+//	if (!cp_env)
+//		return (1);
 	while (42)
 	{
 		input = readline("minishell> ");  
@@ -129,13 +141,13 @@ int main(int argc, char **argv, char **envp)
 		printf("[MAIN]You entered: %s\n\n", input);
 		if (lexer(input, &lex_list)) // it means that a malloc failed, my lex_clean cleaned input and list
 			return (1);
-		/*i = 1;
+		i = 1;
     	while (lex_list)
     	{
 			printf("node %i -- content: %s, type; %i\n", i, lex_list->cont, lex_list->token); //erase
        	 	i++;
         	lex_list = lex_list->next; 
-    	}*/
+    	}
 	}
 	lex_clean(&lex_list, &input);
 	return (0);
