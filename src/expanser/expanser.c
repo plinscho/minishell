e80f3ba315557fd6e8dee66f5049bac7eead402a
U-JIMCAR\marita <marita@MARITA-PC.JIMCAR.local>
2023-11-21 14:01:43 +0100

    continued with w_syntax. Talk to Dina

--- src/expanser/expanser.c
+++ src/expanser/expanser.c
@@ -32,13 +32,29 @@ int		get_words(t_lexer *lex)
 	tmp = lex;
 	while (tmp)
 	{
+		// Maybe we can use another number for the words that will be comands
 		if (lex->token == '1')
 			check = check_word(tmp->cont);
+		
+		//	I made up this token number for the $ operators. Tell Dina.
+		else if (lex->token == 10)
+			check = check_var(tmp->cont);
 		tmp = tmp->next;
 	}
 	
-		
+	// This function returns the exit code. 
+	// It returns to w_syntax so that it can save the error in the sh.
+	return (check);
 }
+
+/*
+	This function checks for syntax errors.
+	The ideal would be to use it after the lexer, so that we know
+	which words are comands and which are just file names or input.
+	w_syntax also gets a error code from get_words and if != 0, 
+	that means some syntax error is made.
+	We can use that to populate sh->exit.
+*/
 int		w_syntax(t_mini *sh)
 {
 	int	words;
