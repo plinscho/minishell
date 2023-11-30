544cb59e64c2687636b7d0770efb867ed793eb95
Nadezhda Zhuzhleva <nzhuzhle@car13s6.42barcelona.com>
2023-11-30 20:20:05 +0100

    started expansion

--- src/expanser/expanser.c
+++ src/expanser/expanser.c
@@ -6,7 +6,7 @@
 /*   By: nzhuzhle <nzhuzhle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 21:18:38 by plinscho          #+#    #+#             */
-/*   Updated: 2023/11/30 19:28:08 by nzhuzhle         ###   ########.fr       */
+/*   Updated: 2023/11/30 20:18:51 by nzhuzhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ char	*expand_str(char *cont)
 	char	*new;
 
 	i = 0;
-	i
 	while (cont[i] && cont[i + 1])
 	{
 		if (cont[i] == '$' && check_chr(cont[i + 1]) > 1)
@@ -41,6 +40,9 @@ char	*expand_str(char *cont)
 */
 int	expanser(t_mini *sh, t_lexer *head)
 {
+	int	flag;
+
+	flag = 0;
 	while (sh->lex_lst)
 	{
 		if (sh->lex_lst->token == 3 && sh->lex_lst->cont)
@@ -49,11 +51,15 @@ int	expanser(t_mini *sh, t_lexer *head)
 			if (!sh->lex_lst->cont)
 				return (err_break(sh_restore(&sh, head, NULL), "malloc", NULL, 12));
 		}
-		else if (sh->lex_lst->token == 1)
+		else if (sh->lex_lst->token == 1 && !flag)
 		{	
 			if (!expand_word(&sh->lex_lst))
 				return (err_break(sh_restore(&sh, head, NULL), "malloc", NULL, 12));
 		}
+		else if (sh->lex_lst->token > 3 && sh->lex_lst->token < 8)
+			flag = 1;
+		if (sh->lex_lst->token > 0 && sh->lex_lst->token < 4 && flag)
+			flag = 0;
 		sh->lex_lst = sh->lex_lst->next;
 	}
 	sh_restore(&sh, head, NULL);
