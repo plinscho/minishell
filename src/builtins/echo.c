2146a35555eb0782c76ca0d033ffbc6b99e38dc8
Paulus Linschoten Framis <plinscho@car7s6.42barcelona.com>
2023-12-04 19:32:36 +0100

    wrote some echo cases

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: plinscho <plinscho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 16:41:54 by plinscho          #+#    #+#             */
-/*   Updated: 2023/11/25 16:51:11 by plinscho         ###   ########.fr       */
+/*   Updated: 2023/12/04 19:31:55 by plinscho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,19 @@
 
 // ECho is a tricky function. Can be used almost everywhere and with -n extension.
 // Im saving it for the last so that can work with the rest of the code when debugged.
+
+/*
+	CASES:
+
+	bash-3.2$ echo -n howdy
+	howdybash-3.2$
+
+	bash-3.2$ echo -nnnnnnnnnn howdy -n
+	howdy -nbash-3.2$
+
+	bash-3.2$ echo -n -n -n -n howdy -n -nnnnnnnnn
+	howdy -n -nnnnnnnnnbash-3.2$
+*/
 
 int		ft_echo(t_mini *sh)
 {
